db504d82fc511927cd36cf6ec9c21a4c8286d0b0
mkdesign <me@mkdesign.ir>
2012-11-25 20:23:15 +0330

    adding comment

--- scripts/myscripts.js
+++ scripts/myscripts.js
@@ -1,18 +1,15 @@
 $(function(){
 	
-	var lists       = $('section.container div.hollinks ol.lists > a'),
-		flechs		= $('section.container div.hollinks ol.lists > div'),
-		flchLength  = flechs.length;
-		listsLength = lists.length;
+	var lists       = $('section.container div.hollinks ol.lists > a'),//(zepto) $ mige :JS====>document.getElementby...getElementByTagName
+		flechs		= $('section.container div.hollinks ol.lists > div');
 
-	lists.mouseover(function(){
-		var that  = this;
-		var index = $(this).index();
+	lists.mouseover(function(){ // (zepto) tuie javascript mishe onmouseover
+		var index = $(this).index();  //return mikone shomare oon farzando.az sefr shoru mishe.bala k ma tage haie "a" ro gereftim 7 ta hastan.in shomarasho mide k kodum hover shodan.
 
 		if(index==6) {
-			flechs.eq(0).animate({
+			flechs.eq(0).animate({// (zepto)eq mige item shomare chand agar yadet bashe bala goftam 7 tas.in mige kodum flch rotate she... animate style mide monteha ba transition.
 				rotate : "-180deg"
-			},250);
+			},250);        //animation duration
 			flechs.eq(1).animate({
 				rotate : "75deg"
 			},250);
@@ -71,7 +68,7 @@ $(function(){
 		}
 	});
 
-	lists.mouseout(function(){
+	lists.mouseout(function(){// tuie javascript mishe onmouseout,kolle function dare hameie flch haro b defult k 0 bashe barmigardune
 		for(i=0;i<6;i++){
 			flechs.eq(i).animate({
 
